a5ee541c04a959d34a98468bdff2a80bb2f6e38a
Gennadiy <ghena187@gmail.com>
2021-06-04 16:01:50 -0500

    add Group5Assignment

---
+++ src/test/java/com/cybertek/groupStadies/May29/Group5Assignment.java
@@ -0,0 +1,43 @@
+package com.cybertek.groupStadies.May29;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Group5Assignment {
+    WebDriver driver;
+    @BeforeMethod
+    public void Group(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("https://qa3.vytrack.com/user/login");
+    }
+    @Test
+    public void group_task() throws InterruptedException {
+        WebElement userName = driver.findElement(By.xpath("//input[@type='text']"));
+        userName.sendKeys("user15");
+
+        WebElement password = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+        password.sendKeys("UserUser123" + Keys.ENTER);
+
+        //Thread.sleep(2000);
+        //WebElement closeButton = driver.findElement(By.xpath("(//button[@type='button'])[2]"));
+        //closeButton.click();
+
+        WebElement fleet = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
+        fleet.click();
+        WebElement options = driver.findElement(By.xpath("(//span[@class='title title-level-2'])[1]"));
+        options.click();
+
+
+
+
+    }
+}
